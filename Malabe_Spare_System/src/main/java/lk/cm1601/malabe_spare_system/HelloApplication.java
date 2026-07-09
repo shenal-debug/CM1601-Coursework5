9f3b49ce58c82812574681bb373374e6cd1d5832
Shenal <shenal.bimalsha@gmail.com>
2026-07-10 00:36:00 +0530

    Implemented inventory file reading

--- Malabe_Spare_System/src/main/java/lk/cm1601/malabe_spare_system/HelloApplication.java
+++ Malabe_Spare_System/src/main/java/lk/cm1601/malabe_spare_system/HelloApplication.java
@@ -4,16 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lk.cm1601.malabe_spare_system.filehandler.InventoryFileHandler;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
+
+        // Read inventory file when the application starts
+        InventoryFileHandler inventoryFileHandler = new InventoryFileHandler();
+        inventoryFileHandler.readInventoryFile();
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        stage.setTitle("Malabe Spare System");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
--- Malabe_Spare_System/src/main/java/lk/cm1601/malabe_spare_system/filehandler/InventoryFileHandler.java
+++ Malabe_Spare_System/src/main/java/lk/cm1601/malabe_spare_system/filehandler/InventoryFileHandler.java
@@ -1,4 +1,34 @@
 package lk.cm1601.malabe_spare_system.filehandler;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class InventoryFileHandler {
+
+    public void readInventoryFile() {
+
+        try {
+
+            BufferedReader reader = new BufferedReader(
+                    new FileReader("src/main/resources/data/inventory_legacy.txt"));
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                System.out.println(line);
+
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+
+            System.out.println("File cannot be read.");
+
+        }
+
+    }
+
 }
